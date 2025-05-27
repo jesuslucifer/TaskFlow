d9a9a6dee9d0791e60de3172e4670d36e75c2a92
GaikIt <110398915+GaikIt@users.noreply.github.com>
2025-05-27 21:33:15 +0300

    User processing: current and by id

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,18 +1,22 @@
 package com.example.backend.controller;
 
 import com.example.backend.model.User;
+import com.example.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/me")
     public ResponseEntity<?> getCurrentUser() {
@@ -30,4 +34,41 @@ public class UserController {
                         "email", user.getEmail(),
                         "avatarUrl", user.getAvatarUrl()));
     }
+
+    @PostMapping("/avatar")
+    public ResponseEntity<String> updateAvatarUrl(@RequestParam("file") MultipartFile avatarUrlRequest) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(401).body("You are not authenticated");
+        }
+
+        User user = (User) authentication.getPrincipal();
+
+        userService.updateAvatar(user.getId(), avatarUrlRequest);
+
+        return ResponseEntity.ok("Successfully updated avatar url");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUser(@PathVariable Long id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(401).body("You are not authenticated");
+        }
+
+        User user = userService.getById(id);
+
+        if (user == null) {
+            return ResponseEntity.status(401).body("User not found");
+        }
+
+        return ResponseEntity.ok(Map.of
+                ("username", user.getUsername(),
+                        "email", user.getEmail(),
+                        "avatarUrl", user.getAvatarUrl()));
+    }
+    
 }
---
+++ backend/src/main/java/com/example/backend/service/LocalStorageService.java
@@ -0,0 +1,37 @@
+package com.example.backend.service;
+
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class LocalStorageService {
+
+    @Value("${backend.upload.dir}")
+    private String uploadDir;
+
+    public String uploadFile(MultipartFile file, String fileName) {
+        try {
+            Path path = Paths.get(uploadDir);
+
+            if (!Files.exists(path)) {
+                Files.createDirectories(path);
+            }
+
+            Path filePath = path.resolve(fileName);
+            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+            return "/uploads/" + fileName;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
