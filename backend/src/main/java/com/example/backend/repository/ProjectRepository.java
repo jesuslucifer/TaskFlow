fca3dfafe3363029da47d604ecde5b05e0b72e0c
jesuslucifer <mit.com254@gmail.com>
2025-06-30 14:01:35 +0300

    fix: fixed notification logic and add notification of 3 days and 1 hour

--- backend/src/main/java/com/example/backend/repository/ProjectRepository.java
+++ backend/src/main/java/com/example/backend/repository/ProjectRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,7 +15,6 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     List<Project> findAllByCreateUserId(Long userId);
     Boolean existsByCreateUserIdAndName(Long userId, String name);
     Optional<Project> findByCreateUserIdAndName(Long userId, String name);
-
-    @EntityGraph(attributePaths = {"executors", "createUser"})
-    List<Project> findAllByDateToBetween(LocalDate dateToAfter, LocalDate dateToBefore);
+    List<Project> findAllByDateToIs(LocalDate dateTo);
+    List<Project> findAllByDateToAndTimeLeftBetween(LocalDate dateTo, LocalTime timeLeftAfter, LocalTime timeLeftBefore);
 }
--- backend/src/main/java/com/example/backend/service/impl/DeadlineNotificationServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/impl/DeadlineNotificationServiceImpl.java
@@ -1,48 +1,89 @@
 package com.example.backend.service.impl;
 
+import com.example.backend.model.NotificationHistory;
 import com.example.backend.model.Project;
+import com.example.backend.repository.NotificationHistoryRepository;
 import com.example.backend.repository.ProjectRepository;
 import com.example.backend.service.DeadlineNotificationService;
+import com.example.backend.service.NotificationHistoryService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DeadlineNotificationServiceImpl implements DeadlineNotificationService {
     private final ProjectRepository projectRepository;
     private final SimpMessagingTemplate messagingTemplate;
+    private final NotificationHistoryRepository notificationHistoryRepository;
+    private final NotificationHistoryService notificationHistoryService;
 
     @Scheduled(fixedRate = 30000)
     @Transactional
     public void checkDeadlines() {
-        //checkDeadlinesOfDays(3);
+        checkDeadlinesOfDays(3);
         checkDeadlinesOfDays(1);
+        checkDeadlinesOfHours(1);
     }
 
 
     private void checkDeadlinesOfDays(int days) {
         LocalDate dateNow = LocalDate.now();
+        String typeNotification = "deadline_days";
 
-        LocalDate dateTomorrow = dateNow.plusDays(days + 1);
+        LocalDate dateTomorrow = dateNow.plusDays(days);
 
-        List<Project> projects = projectRepository.findAllByDateToBetween(dateNow, dateTomorrow);
+        List<Project> projects = projectRepository.findAllByDateToIs(dateTomorrow);
 
+        sendNotification(projects, typeNotification,  String.valueOf(days));
+    }
+
+    private void checkDeadlinesOfHours(int hours) {
+        LocalDate dateNow = LocalDate.now();
+        LocalTime now = LocalTime.now();
+        String typeNotification = "deadline_hours";
+
+        List<Project> projects = projectRepository.findAllByDateToAndTimeLeftBetween(dateNow, now, now.plusHours(hours));
+
+        sendNotification(projects, typeNotification,  String.valueOf(hours));
+    }
+
+    private void sendNotification(List<Project> projects, String typeNotification, String periodNotification) {
         projects.forEach(project -> {
-            String message = String.format("До дедлайна проекта %s осталось менее %d дня", project.getName(), days);
-            messagingTemplate.convertAndSendToUser(project.getCreateUser().getUsername(),
+            if (!notificationHistoryRepository.existsByProjectIdAndTypeNotificationAndPeriodNotification(
+                    project.getId(),
+                    typeNotification,
+                    periodNotification)) {
+                String message = String.format("Скоро дедлайн у проекта %s", project.getName());
+                sendNotificationToUser(project.getCreateUser().getUsername(), message);
+                project.getExecutors().forEach(executor -> {
+                    sendNotificationToUser(executor.getUser().getUsername(), message);
+                });
+                notificationHistoryService.save(NotificationHistory.builder()
+                        .project(project)
+                        .typeNotification(typeNotification)
+                        .periodNotification(periodNotification)
+                        .build());
+            }
+        });
+    }
+
+    private void sendNotificationToUser(String userName, String message) {
+        try {
+            messagingTemplate.convertAndSendToUser(userName,
                     "/queue/deadline-notification",
                     message);
-            project.getExecutors().forEach(executor -> {
-                messagingTemplate.convertAndSendToUser(executor.getUser().getUsername(),
-                        "/queue/deadline-notification",
-                        message);
-            });
-        });
+            log.info("Уведомление отправлено пользователю {}: {}", userName, message);
+        } catch (Exception e) {
+            log.error("Ошибка при отправке уведомления пользователю {}", userName, e);
+        }
     }
 }
