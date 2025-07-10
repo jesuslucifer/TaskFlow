64e7ef32aff107583a50c3bb3d65edc13122c884
jesuslucifer <mit.com254@gmail.com>
2025-07-10 11:30:15 +0300

    refactor: delete exception thrown in methods

--- backend/src/main/java/com/example/backend/controller/AuthController.java
+++ backend/src/main/java/com/example/backend/controller/AuthController.java
@@ -32,7 +32,7 @@ public class AuthController {
     private final TokenService tokenService;
 
     @PostMapping("/login")
-    public ResponseEntity<JwtResponse> login(@RequestBody SignInRequest signInRequest) throws Exception {
+    public ResponseEntity<JwtResponse> login(@RequestBody SignInRequest signInRequest) {
         try {
             User user = userService.getByUsernameOrEmail(signInRequest.getUsernameOrEmail());
 
@@ -54,7 +54,7 @@ public class AuthController {
     }
 
     @PostMapping("/sign-up")
-    public ResponseEntity<?> signUp(@RequestBody SignUpRequest signUpRequest) throws Exception {
+    public ResponseEntity<?> signUp(@RequestBody SignUpRequest signUpRequest) {
         var user = User.builder()
                 .username(signUpRequest.getUsername())
                 .email(signUpRequest.getEmail())
@@ -72,7 +72,7 @@ public class AuthController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<?> refreshToken(@RequestBody RefreshTokenRequest request) throws Exception {
+    public ResponseEntity<?> refreshToken(@RequestBody RefreshTokenRequest request) {
         String requestRefreshToken = request.getRefreshToken();
 
         String username = jwtService.extractUsername(requestRefreshToken);
